eb45e5dec99591f1325528ee699516fa4fe045e9
TheBest1022 <fernando07101@outlook.com>
2023-05-18 00:53:42 -0500

    Actualizar

--- src/config/configuracion.js
+++ src/config/configuracion.js
@@ -1,7 +1,14 @@
 /*export const dataConfig = {
   host: "us-cdbr-east-06.cleardb.net",
-  user: "bf576db198a75d",
+  user: "bf576db198a75d:",
   password: "25eb97f8",
   database: "heroku_33979b66acbea7f",
   port: "3306",
-};
+};*/
+export const dataConfig = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bda_app",
+  port: "3307"
+}
