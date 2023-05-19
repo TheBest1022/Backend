05c71e87f777022f19412d14356bb0aef4e8f446
TheBest1022 <fernando07101@outlook.com>
2023-05-19 12:38:31 -0500

    Actualizar

--- src/config/configuracion.js
+++ src/config/configuracion.js
@@ -1,8 +1,8 @@
 /*export const dataConfig = {
   host: "us-cdbr-east-06.cleardb.net",
-  user: "bf576db198a75d",
-  password: "25eb97f8",
-  database: "heroku_33979b66acbea7f",
+  user: "b0c17e5378b8aa",
+  password: "359e3c83",
+  database: "heroku_86ba5800333751b",
   port: "3306",
 };*/
 export const dataConfig = {
