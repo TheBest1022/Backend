fbd48cd7884f360e783a37956b4a2efeb95d0484
TheBest1022 <fernando07101@outlook.com>
2023-07-19 12:45:28 -0500

    Actualizar

--- src/config/configuracion.js
+++ src/config/configuracion.js
@@ -1,16 +1,6 @@
-/*
- */
 
 /*
-export const dataConfig = {
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "b0c17e5378b8aa",
-  password: "359e3c83",
-  database: "heroku_86ba5800333751b",
-  port: "3306",
-};
-
-*/
+--SERVIDOR RAILWAY
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
@@ -23,4 +13,22 @@ export const dataConfig = {
   password: DB_PASSWORD,
   database: DB_DATABASE,
   port: DB_PORT,
+};
+
+export const dataConfig = {
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b0c17e5378b8aa",
+  password: "359e3c83",
+  database: "heroku_86ba5800333751b",
+  port: "3306",
+};
+*/
+
+
+export const dataConfig = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "heroku_app",
+  port: "3307",
 };
